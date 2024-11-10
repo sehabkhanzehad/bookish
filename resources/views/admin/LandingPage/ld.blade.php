3136fd47a6e45de0f0773c6a82a1815a33620d8b
sehabkhanzehad <sehabkhanzehad@yahoo.com>
2024-11-10 18:21:44 +0600

    add

--- resources/views/admin/LandingPage/ld.blade.php
+++ resources/views/admin/LandingPage/ld.blade.php
@@ -9,8 +9,8 @@
     <meta name="keywords" content="">
 
     <!-- Favicons -->
-    <link href="{{ asset('landing') }}/img/favicon.png" rel="icon">
-    <link href="{{ asset('landing') }}/img/apple-touch-icon.png" rel="apple-touch-icon">
+    {{-- <link href="{{ asset('landing') }}/img/favicon.png" rel="icon">
+    <link href="{{ asset('landing') }}/img/apple-touch-icon.png" rel="apple-touch-icon"> --}}
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
